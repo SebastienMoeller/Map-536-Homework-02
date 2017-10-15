1c1be0ba101876e94ffd37b6f0d8178cf28ba9bb
MoellerSebs <Sebastien@Sebastiens-MacBook-Air.local>
2017-10-15 15:02:52 +0200

    draw_a_name updated

--- R/draw_a_name.R
+++ R/draw_a_name.R
@@ -16,13 +16,13 @@ draw_a_name <- function(the_name, the_sex){
   assert_that(the_sex %in% c("M", "F"))
   assert_that(is.string(the_name))
 
-  tmp <- prenoms::prenoms %>%
+  the_plot <- prenoms::prenoms %>%
     filter(sex==the_sex, name==the_name) %>%
-    group_by(year,sex,name) %>%
-    summarize(n=sum(n)) %>%
-    ggplot(aes(x=year,y=n)) +
+    group_by(year, sex,name) %>%
+    summarize(n = sum(n)) %>%
+    ggplot(aes(x = year, y = n)) +
     geom_line() +
-    labs(title=the_name) +
+    labs(title = the_name) +
     theme_gdocs()
-  return(tmp)
+  return(the_plot)
 }
